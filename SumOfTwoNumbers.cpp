8a6c82b0f4ed346bf8da8cfef38fd29bd3f824a1
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-05 09:37:33 -0700

    Add files via upload

---
+++ SumOfTwoNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Num1, Num2;
+
+    cout << "What is your first number?\n";
+    cin >> Num1;
+    cout << "\nWhat is your second number?\n";
+    cin >> Num2;
+
+    int Sum = Num1 + Num2;
+
+    cout << "\nThe sum of " << Num1 << " and " << Num2 << " is : " << Sum << "\n\n";
+}
