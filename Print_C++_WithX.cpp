8ea276a26d34fa9f02de768b78f71f34902a6dc5
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-06 10:33:44 -0700

    Add files via upload

---
+++ Print_C++_WithX.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\n\n Print the following pattern:\n";
+	cout << "--------------------------------\n";
+	cout << " xxxxx\n";
+	cout << "x     x       x        x\n";
+	cout << "x             x        x\n";
+	cout << "x          xxxxxxx  xxxxxxx\n";
+	cout << "x             x        x\n";
+	cout << "x     x       x        x\n";
+	cout << " xxxxx\n";
+	cout << "--------------------------------\n";
+}
